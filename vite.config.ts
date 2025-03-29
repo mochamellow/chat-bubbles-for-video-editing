b4a7318b229d4254ce5a6aec0e46889fd3b7b91e
Gilbert Aligoey <gil@Gilberts-MacBook-Pro.local>
2025-03-29 18:20:34 +0800

    Update base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/chat-bubbles-for-video-editing/', // 👈 Replace with your GitHub repo name
   server: {
     port: 3000
   },
